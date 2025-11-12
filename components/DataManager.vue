2f1736e1079d65c73277115fb2fbb2ef23d934f2
moez <moez.kacem@keyrus.com>
2025-11-12 17:58:00 +0100

    Update DataManager.vue

--- components/DataManager.vue
+++ components/DataManager.vue
@@ -211,6 +211,8 @@ const clearAllData = () => {
     toast.add({ title: 'Data cleared', description: 'All data removed', color: 'error' })
     // Navigate to today after clearing data
     navigateToToday()
+    // Ensure Load Sample button is shown
+    store.hasData = false
   }
 }
 
