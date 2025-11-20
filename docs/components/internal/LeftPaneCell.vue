e06d9e54b8685b078293d4cb629ef8321bd07d7e
moez <moez.kacem@keyrus.com>
2025-11-20 11:53:40 +0100

    update doc

--- docs/components/internal/LeftPaneCell.vue
+++ docs/components/internal/LeftPaneCell.vue
@@ -1,10 +1,12 @@
 <template>
   <div
     :class="[
-      'flex items-center gap-2 h-full px-3 py-2 text-sm',
+      'flex items-center gap-2 h-full px-3 pl-8 py-2 text-sm',
       isAdd ? 'text-slate-600 bg-slate-50 hover:bg-slate-100 cursor-pointer' : 'text-slate-800 cell-text'
     ]"
-    @click="onClick"
+    style="-webkit-user-select: none; user-select: none;"
+    draggable="false"
+    @click="$emit('click')"
   >
     <!-- <div v-if="isAdd" class="shrink-0 inline-flex items-center justify-center w-5 h-5 rounded border border-slate-300 text-slate-700">+</div> -->
     <div class="min-w-0">
@@ -23,15 +25,11 @@
 </template>
 
 <script setup lang="ts">
-const emit = defineEmits<{ (e: 'click'): void }>()
 defineProps<{
   title: string
   subtitle?: string
   isAdd?: boolean
 }>()
-
-function onClick() {
-  emit('click')
-}
+defineEmits<{ (e: 'click'): void }>()
 </script>
 
